61f2a63d38d19e81efd83278e1722cd94e330e33
princebanga <59930965+princebanga@users.noreply.github.com>
2026-06-24 15:36:45 +0530

    Update projects.js

--- projects.js
+++ projects.js
@@ -7,7 +7,7 @@ const projectsData = [
     { y: 668.5, x: 1212.4, name: "Tarang Atlantis", link: "https://www.youtube.com/watch?v=M8R6RR9EKp8", cat: "High-Rise", price: "approx 1.5 cr" },
     { y: 447.6, x: 1033.8, name: "Coral Park", link: "https://www.youtube.com/shorts/FFJFlOx2m_w", cat: "Plots", price: "approx 1.40 Lakh per sq yard" },
     { y: 450.3, x: 989.4, name: "NeoLiv", link: "https://www.youtube.com/shorts/9l6LcFpALBY", cat: "Plots", price: "approx 2 lakh per sq yard" },
-    { y: 558.6, x: 805.5, name: "Townfit 70", link: "https://www.youtube.com/watch?v=TL2Abh_JxvA", cat: "Plots", price: "1.40 Lakh per sq yard" },
+    { y: 558.6, x: 805.5, name: "Townfit 70", link: "https://faridabadmasterplan.in/townfit-project.html", cat: "Plots", price: "1.40 Lakh per sq yard" },
     { y: 440.1, x: 1043.0, name: "SOHA Olive Town", link: "https://www.youtube.com/watch?v=FdvkILluDas", cat: "Plots", price: "1.45 Lakh per sq yard" },
     { y: 361.6, x: 642.0, name: "Dameera Plots", link: "https://faridabadmasterplan.in/dameera.html", cat: "Plots", price: "approx 75k per sq yard" }, // वापस जोड़ा गया
     { y: 488.3, x: 927.0, name: "BPTP Parkland Pride", link: "https://www.youtube.com/shorts/7n0Kss_6mJs", cat: "Low-Rise", price: "2.15 lakh per sq yard" },
