2ad2a55c2de54b1cc11931f44d06ca598dc9b261
princebanga <59930965+princebanga@users.noreply.github.com>
2026-06-26 14:23:30 +0530

    Update menu.js

--- menu.js
+++ menu.js
@@ -126,6 +126,8 @@ const globalMenuHTML = `
                             <a href="ongoing-plots.html">Ongoing Projects <span class="sub-arrow">▸</span></a>
                             <div class="sub-dropdown-content">
                                 <a href="dameera.html">DaMeera City</a>
+                                
+                                <a href="townfit-project.html">TownFit70</a>
                             </div>
                         </div>
                         <a href="upcoming-plots.html">Upcoming Projects</a>
