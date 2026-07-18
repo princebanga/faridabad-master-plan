379312b0261dc6358dc44fbcefc85415ff2784b6
princebanga <59930965+princebanga@users.noreply.github.com>
2026-07-18 18:40:20 +0530

    Remove sub-dropdown from Group Housing menu
    Removed sub-dropdown for Rera approved Maple floors.

--- menu.js
+++ menu.js
@@ -139,10 +139,7 @@ const globalMenuHTML = `
                     <a href="javascript:void(0)" class="dropbtn">Group Housing ▾</a>
                     <div class="dropdown-content">
                         <a href="runningprojects.html">Ongoing Projects</a>
-                        <div class="sub-dropdown">
-                                <a href="gulmohar-maple.html">Rera approved Maple floors</a>
-                            </div>
-                            </div>
+                        
                             <a href="upcoming.html">Upcoming Projects <span class="sub-arrow">▸</span></a>
                             <div class="sub-dropdown-content">
                                 <a href="emerald-sector-87-faridabad.html">Emerald Low-Rise Sector 87</a>
